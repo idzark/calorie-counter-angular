c63825eea4cc0305d44b2fe960459472daa5bab1
idzark <a.idzikowski91@gmail.com>
2018-01-28 18:52:15 +0100

    add styles for login and register components

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  username: string;
+  password: string;
+  passwordConfirmation: string;
 
   constructor() { }
 
